afcd67303a6a74f3f05750518e7b7bcff9bac90d
Rickard Larsson <rickard.larsson.94@gmail.com>
2017-11-22 16:40:07 +0100

    Uncoded transmission fully functional

--- project_plain.m
+++ project_plain.m
@@ -1,22 +1,22 @@
 % ======================================================================= %
 % SSY125 Project
 % ======================================================================= %
+profile on
 clc
 clear
 
 % ======================================================================= %
 % Simulation Options
 % ======================================================================= %
-N = 1e5;  % simulate N bits each transmission (one block)
-maxNumErrs = 100; % get at least 100 bit errors (more is better)
-maxNum = 1e7; % OR stop if maxNum bits have been simulated
-EbN0 = -1:0.5:12; % power efficiency range
+N = 1e5;                % simulate N bits each transmission (one block)
+maxNumErrs = 100;       % get at least 100 bit errors (more is better)
+maxNum = 1e7;           % OR stop if maxNum bits have been simulated
+EbN0 = -1:0.5:12;       % power efficiency range
 
+EsN0 = EbN0 + 10*log10(2);
+SNRlin = 10.^(EsN0/10);
 constQPSK = [(1 + 1i) (1 - 1i) (-1 + 1i) (-1 - 1i)]/sqrt(2);
-% ======================================================================= %
-% Other Options
-% ======================================================================= %
-% ...
+
 
 % ======================================================================= %
 % Simulation Chain
@@ -37,8 +37,9 @@ for i = 1:length(EbN0) % use parfor ('help parfor') to parallelize
       symb = constQPSK(msgIdx); 
 
       % [CHA] add Gaussian noise
-      noise = randn(1,N/2) + 1i*randn(1,N/2);
-      noiseSymb = 2*10^(EbN0(i)/10)*symb + noise;
+      sigma = 1/(sqrt(2*SNRlin(i)));
+      noise = sigma*(randn(1,N/2) + 1i*randn(1,N/2));
+      noiseSymb = symb + noise;
 
       % [HR] Hard Receiver
       noiseSymb(real(noiseSymb)>0 & imag(noiseSymb)>0) = 1;
@@ -58,12 +59,20 @@ for i = 1:length(EbN0) % use parfor ('help parfor') to parallelize
 %           num2str(num) '/' num2str(maxNum) ' bits. Projected error rate = '...
 %           num2str(totErr/num, '%10.1e') '. +++']);
   end 
+  
   BER(i) = totErr/num; 
+  
 end
-semilogy(EbN0,BER)
+
+semilogy(EbN0,BER,'or')
+hold on 
+theoryBER = 0.5*erfc(sqrt(10.^(EbN0/10)));
+semilogy(EbN0,theoryBER,'b')
+legend('Simulation', 'Theory')
+title('BER vs. E_{b}/N_{0} for uncoded QPSK')
 xlabel('E_{b}/N_{0} [dB]')
 ylabel('BER')
-axis([-1 12 10^-5 1])
+axis([-1 10 10^-5 1])
 % ======================================================================= %
 % End
 % ======================================================================= %
